ac96e8c1b4102b40d02d3bc685b3549e8a612cd7
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-16 22:54:20 +0100

    Routie geïmplementeerd

--- scripts/main.js
+++ scripts/main.js
@@ -2,38 +2,43 @@ import { $ } from "./modules/$.js"
 import { get } from "./modules/get.js"
 import "./modules/vendor/routie.min.js"
 
-// Get the products from the API when the hash changes.
+// Get the results from the API when the hash changes.
 routie ({
-    ":hash": hash => {
+    ":tab": tab => {
+        get(true, tab)
     }
 })
 
-// Variable which keeps track of the current tab.
-let tab = "Veganistische+kookboeken"
-
 window.onload = function() {
-    get(true, tab)
+    // If the hash is empty, add the default tab as the hash.
+    if (!window.location.hash) {
+        window.location.hash = "Veganistische+kookboeken"
+    }
 
-    $(".cooking").classList.add("green")
+    // Color the tabs according to the selected one (found in the hash).
+    if (window.location.hash.substring(1) == "Veganisme") {
+        $(".cooking").classList.add("grey")
+        $(".philosophical").classList.add("green")
+    } else {
+        $(".cooking").classList.add("green")
+    }
 }
 
 // EventListeners for tabs.
 $(".cooking").addEventListener("click", function() {
-    tab = "Veganistische+kookboeken"
-    get(true, tab)
+    window.location.hash = "Veganistische+kookboeken"
 
     $(".cooking").classList.remove("grey")
     $(".philosophical").classList.remove("green")
 })
 
 $(".philosophical").addEventListener("click", function() {
-    tab = "Veganisme"
-    get(true, tab)
+    window.location.hash = "Veganisme"
 
     $(".cooking").classList.add("grey")
     $(".philosophical").classList.add("green")
 })
 
 $(".more").addEventListener("click", function() {
-    get(false, tab)
+    get(false, window.location.hash.substring(1))
 })
